dde96b14ca9dc9352ce47d13e43a58ef31c0c403
egomenn <egemen.kalfa@gmail.com>
2019-01-09 22:03:00 +0300

    adasd

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,9 +12,7 @@ class Quiz extends Component {
     render() {
         return (
             <div>
-                <div className="QuizQuestion">
-                    <QuizQuestion quizQuestion={quizData.quizQuestions[this.state.quiz_position - 1]} />
-                </div>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div>
         );
     }
